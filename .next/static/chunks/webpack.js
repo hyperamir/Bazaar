268d1b682e68b5154bff3bc59b229bfe7de5fc3e
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-05-11 14:33:14 -0400

    product/slug implementation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f7991453423f3304"; }
+/******/ 		__webpack_require__.h = function() { return "c0ae24088b67dc39"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
